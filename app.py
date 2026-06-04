e7b30740476a78d72e439cb3543415c9da51cbf6
FaboCloudsurf <FaboCloudsurf@gmail.com>
2026-06-03 22:14:37 -0400

    web hook test2

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def hello_world():
     return 'Hello from Dockerized Flask App!'
 
 if __name__ == '__main__':
-    # Host '0.0.0.0' allows the app to accept connections from outside its containers
+    # Host '0.0.0.0' allows the app to accept connections from outside its container
     app.run(host='0.0.0.0', port=5001)
 
 
+#webhook test 2
